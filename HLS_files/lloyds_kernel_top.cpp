ec77e4ca5066fbed912b3d4580a75c26194ed480
Jukka Hertzog <jjh113@ic.ac.uk>
2017-06-20 11:27:24 +0100

    Updated HLS kernel to be less vulnerable to optimisations

--- HLS_files/lloyds_kernel_top.cpp
+++ HLS_files/lloyds_kernel_top.cpp
@@ -12,24 +12,6 @@
 #include "lloyds_util.h"
 
 
-
-//void load_points_buffer(uint offset,uint address, volatile bus_type *bus, data_type *buffer)
-//{
-//
-//	bus_type int_buffer[B*D];
-//
-//	memcpy(int_buffer, (const bus_type *)(bus + (offset + address)/sizeof(bus_type)), B*D*sizeof(bus_type));
-//
-//	for (uint i=0; i<B; i++) {
-//		#pragma HLS loop_flatten
-//		#pragma HLS pipeline II=1
-//		for (uint d=0; d<D; d++) {
-//			//#pragma HLS unroll
-//			buffer[i].value[d] = int_buffer[i*D+d];
-//		}
-//	}
-//}
-
 void load_points_buffer(uint offset,uint address, volatile bus_type *bus, uint *buffer)
 {
 
@@ -45,42 +27,6 @@ void load_points_buffer(uint offset,uint address, volatile bus_type *bus, uint *
 	}
 }
 
-//void load_centres_buffer(uint offset, uint address, volatile bus_type *bus, centre_index_type k, data_type *buffer)
-//{
-//	bus_type int_buffer[K*D];
-//
-//	memcpy(int_buffer, (const bus_type *)(bus + (offset + address)/sizeof(bus_type)), (k+1)*D*sizeof(bus_type));
-//
-//	for (centre_index_type i=0; i<=k; i++) {
-//		#pragma HLS loop_flatten
-//		#pragma HLS pipeline II=1
-//		for (uint d=0; d<D; d++) {
-//			//#pragma HLS unroll
-//			buffer[i].value[d] = int_buffer[i*D+d];
-//		}
-//		if (i==k)
-//			break;
-//	}
-//}
-
-//void load_centres_buffer(uint offset, uint address, volatile bus_type *bus, centre_index_type k, uint *buffer)
-//{
-//	bus_type int_buffer[K];
-//
-//	memcpy(int_buffer, (const bus_type *)(bus + (offset + address)/sizeof(bus_type)), (k+1)*sizeof(bus_type));
-//
-//	for (centre_index_type i=0; i<=k; i++)
-//	{
-//		#pragma HLS loop_flatten
-//		#pragma HLS pipeline II=1
-//			//#pragma HLS unroll
-//			buffer[i] = int_buffer[i];
-//		if (i==k)
-//		{
-//			break;
-//		}
-//	}
-//}
 
 //store_output_buffer(output_addr, output_buffer, kernel_info_block_address, master_portA);
 void store_output_buffer(uint offset, uint *buffer, uint address, volatile bus_type *bus)
@@ -146,61 +92,242 @@ void lloyds_kernel_top(  uint block_address,
 	#pragma HLS RESOURCE variable=return core=AXI4LiteS	metadata="-bus_bundle CONFIG_BUS"
 
 
-//	data_type data_points_buffer[B];
-//	data_type centres_buffer[K];
-//	output_type output_buffer[B];
-
 	uint data_points_buffer[B];
 	uint centres_buffer[K];
 	uint output_buffer[B];
 
-
-	//void load_points_buffer(uint offset,uint address, volatile bus_type *bus, data_type *buffer)
-	//void load_centres_buffer(uint offset, uint address, volatile bus_type *bus, centre_index_type k, data_type *buffer)
-
-//	uint data_points_block_address = block_address;
-//	uint kernel_info_block_address = block_address;
-
-//	load_points_buffer(data_points_addr, data_points_block_address, master_portA, data_points_buffer);
-//	load_centres_buffer(centres_in_addr, 0, master_portA, k, centres_buffer);
-//	*debug = data_points_buffer[0].value[0];
-
-	/*
-	for (uint i=0; i<B; i++) {
-		data_type u = data_points_buffer[i];
-		printf("%d %d %d\n", u.value[0], u.value[1], u.value[2]);
-	}
-	*/
+	data_points_buffer[0] = 51;
 
 	for(int i = 1; i<B-1; i++)
 	{
-		output_buffer[i] = i;
+		output_buffer[i] = i + n;
 	}
 	output_buffer[0] = n;
 	output_buffer[B-1] = k;
-
+/*
 	block_address = 100;
 	data_points_addr = 5;
 	centres_in_addr = 6;
 	k = 10000;
 	output_buffer[0] = n;
+*/
+	int kernel_info_block_address = 0;
+	store_output_buffer(output_addr, output_buffer, kernel_info_block_address, master_portA);
+}
 
 
 
-    // iterate over all data points
-    //process_data_points_loop: for (uint i=0; i<10; i++) {
 
-    	//data_type u = data_points_buffer[i];
 
-    	/*
-		#ifndef __SYNTHESIS
-			for (uint d=0; d<D; d++) {
-				printf("%d ", u.value[d]);
-			}
-			printf("\n");
-		#endif
-		*/
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///**********************************************************************
+//* Felix Winterstein, Imperial College London
+//*
+//* File: lloyds_kernel_top.cpp
+//*
+//* Revision 1.01
+//* Additional Comments: distributed under a BSD license, see LICENSE.txt
+//*
+//**********************************************************************/
+//
+//#include "lloyds_kernel_top.h"
+//#include "lloyds_util.h"
+//
+//
+//
+//void load_points_buffer(uint offset,uint address, volatile bus_type *bus, data_type *buffer)
+//{
+//
+//	bus_type int_buffer[B*D];
+//
+//	memcpy(int_buffer, (const bus_type *)(bus + (offset + address)/sizeof(bus_type)), B*D*sizeof(bus_type));
+//
+//	for (uint i=0; i<B; i++) {
+//		#pragma HLS loop_flatten
+//		#pragma HLS pipeline II=1
+//		for (uint d=0; d<D; d++) {
+//			//#pragma HLS unroll
+//			buffer[i].value[d] = int_buffer[i*D+d];
+//		}
+//	}
+//}
+//
+//void load_centres_buffer(uint offset, uint address, volatile bus_type *bus, centre_index_type k, data_type *buffer)
+//{
+//	bus_type int_buffer[K*D];
+//
+//	memcpy(int_buffer, (const bus_type *)(bus + (offset + address)/sizeof(bus_type)), (k+1)*D*sizeof(bus_type));
+//
+//	for (centre_index_type i=0; i<=k; i++) {
+//		#pragma HLS loop_flatten
+//		#pragma HLS pipeline II=1
+//		for (uint d=0; d<D; d++) {
+//			//#pragma HLS unroll
+//			buffer[i].value[d] = int_buffer[i*D+d];
+//		}
+//		if (i==k)
+//			break;
+//	}
+//}
+//
+//
+//void store_output_buffer(uint offset, output_type *buffer, uint address, volatile bus_type *bus)
+//{
+//	bus_type int_buffer[B*2];
+//
+//
+//	for (uint i=0; i<B; i++) {
+//
+//		#pragma HLS pipeline II=1
+//		int_buffer[i*2+0] = (bus_type)buffer[i].min_idx;
+//		int_buffer[i*2+1] = (bus_type)buffer[i].sum_sq;
+//
+//	}
+//
+//	memcpy((bus_type *)(bus + (offset + address)/sizeof(bus_type) ), int_buffer, B*2*sizeof(bus_type));
+//}
+//
+//
+//void store_output_points_buffer(uint offset, data_type *buffer, uint address, volatile bus_type *bus)
+//{
+//	bus_type int_buffer[B*D];
+//
+//	for (uint i=0; i<B; i++) {
+//
+//		#pragma HLS loop_flatten
+//		#pragma HLS pipeline II=1
+//		for (uint d=0; d<D; d++) {
+//			//#pragma HLS unroll
+//			int_buffer[i*D+d] = (bus_type)buffer[i].value[d];
+//		}
+//	}
+//
+//	memcpy((bus_type *)(bus + (offset + address)/sizeof(bus_type) ), int_buffer, B*D*sizeof(bus_type));
+//
+//}
+//
+//
+//// top-level function of the design
+//void lloyds_kernel_top(  uint block_address,
+//						 volatile bus_type *master_portA,
+//						 //volatile bus_type *master_portB,
+//						 uint data_points_addr,
+//                         uint centres_in_addr,
+//                         uint output_addr,
+//                         uint update_points,
+//                         uint n,
+//                         uint k, //changed so that the AXI slave interface can be used.
+//			 uint *debug
+//                         )
+//{
+//
+//	// set up the axi bus interfaces
+//	#pragma HLS INTERFACE ap_bus port=master_portA depth=2147483648
+//	#pragma HLS resource variable=master_portA core=AXI4M
+//
+//	//#pragma HLS INTERFACE ap_bus port=master_portB depth=0x002000
+//	//#pragma HLS resource core=AXI4M variable=master_portB
+//
+//	#pragma HLS INTERFACE ap_none register port=data_points_addr
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=data_points_addr metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS INTERFACE ap_none register port=centres_in_addr
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=centres_in_addr metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS INTERFACE ap_none register port=output_addr
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=output_addr metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS INTERFACE ap_none register port=n
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=n metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS INTERFACE ap_none register port=k
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=k metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS INTERFACE ap_none register port=update_points
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=update_points metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS INTERFACE ap_none register port=block_address
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=block_address metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS INTERFACE ap_none register port=debug
+//	#pragma HLS RESOURCE core=AXI4LiteS variable=debug metadata="-bus_bundle CONFIG_BUS"
+//
+//	#pragma HLS RESOURCE variable=return core=AXI4LiteS	metadata="-bus_bundle CONFIG_BUS"
+//
+//	data_type data_points_buffer[B];
+//	data_type centres_buffer[K];
+//	output_type output_buffer[B];
+//	data_type output_points_buffer[B];
+//
+//
+//	//void load_points_buffer(uint offset,uint address, volatile bus_type *bus, data_type *buffer)
+//	//void load_centres_buffer(uint offset, uint address, volatile bus_type *bus, centre_index_type k, data_type *buffer)
+//
+//	uint data_points_block_address = D*block_address;
+//	uint kernel_info_block_address = 2*block_address;
+//
+//	load_points_buffer(data_points_addr, data_points_block_address, master_portA, data_points_buffer);
+//	load_centres_buffer(centres_in_addr, 0, master_portA, k, centres_buffer);
+//	*debug = data_points_buffer[0].value[0];
+//
+//	/*
+//	for (uint i=0; i<B; i++) {
+//		data_type u = data_points_buffer[i];
+//		printf("%d %d %d\n", u.value[0], u.value[1], u.value[2]);
+//	}
+//	*/
+//
+//
+//
+//    // iterate over all data points
+//    process_data_points_loop: for (uint i=0; i<B; i++) {
+//
+//    	data_type u = data_points_buffer[i];
+//
+//    	/*
+//		#ifndef __SYNTHESIS
+//			for (uint d=0; d<D; d++) {
+//				printf("%d ", u.value[d]);
+//			}
+//			printf("\n");
+//		#endif
+//		*/
+//
 //        centre_index_type final_centre_index = 0;
 //        coord_type sum_sq_out = MAX_FIXED_POINT_VAL_EXT;
 //        coord_type min_dist = MAX_FIXED_POINT_VAL_EXT;
@@ -233,26 +360,33 @@ void lloyds_kernel_top(  uint block_address,
 //				break;
 //			}
 //		}
-        /*
-		#ifndef __SYNTHESIS__
-        	printf("%d %d\n",final_centre_index.VAL, sum_sq_out);
-		#endif
-		*/
-
-        //output_buffer[i].min_idx = final_centre_index;
-        //output_buffer[i].sum_sq = sum_sq_out;
-
-
-        //printf("%d %d %d\n", closest_centre.value[0], closest_centre.value[1], closest_centre.value[2]);
-
+//        /*
+//		#ifndef __SYNTHESIS__
+//        	printf("%d %d\n",final_centre_index.VAL, sum_sq_out);
+//		#endif
+//		*/
+//
+//        output_buffer[i].min_idx = final_centre_index;
+//        output_buffer[i].sum_sq = sum_sq_out;
+//
+//        output_points_buffer[i] = closest_centre;
+//
+//        //printf("%d %d %d\n", closest_centre.value[0], closest_centre.value[1], closest_centre.value[2]);
+//
 //	}
-
-    //printf("\n\n");
-
-	int kernel_info_block_address = 0;
-	store_output_buffer(output_addr, output_buffer, kernel_info_block_address, master_portA);
-
-}
-
-
-
+//
+//    //printf("\n\n");
+//
+//	//void store_output_buffer(uint offset, output_type *buffer, uint address, volatile bus_type *bus)
+//    if (update_points == 0) {
+//    	store_output_buffer(output_addr, output_buffer, kernel_info_block_address, master_portA);
+//    } else {
+//    	store_output_points_buffer(output_addr, output_points_buffer, data_points_block_address, master_portA);
+//    }
+//
+//
+//
+//
+//}
+//
+//
